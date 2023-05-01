0743fe88ee81ab13acc18ee9d2b16b5a7a4750b6
Julia Nowicka <julia.nowickaa@gmail.com>
2023-05-01 21:35:34 +0200

    Add the task solution

--- FrogJmp.js
+++ FrogJmp.js
@@ -22,3 +22,9 @@ Write an efficient algorithm for the following assumptions:
 
 X, Y and D are integers within the range [1..1,000,000,000];
 X ≤ Y.*/
+function solution(X, Y, D) {
+
+    const distanceToCover = Y - X;
+    const minimalNumberOfJumps = Math.ceil(distanceToCover / D);
+    return minimalNumberOfJumps;
+}
